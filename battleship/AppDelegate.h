155bf1148b0f875dc07f66567b7970fd2ca35780
Theodore Abshire <TheodoreAbshire@Gmail.com>
2015-12-13 19:14:20 -0800

    Initial Commit

---
+++ battleship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  battleship
+//
+//  Created by Theodore Abshire on 12/13/15.
+//  Copyright © 2015 Roman, Theodore, and Trub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ battleship/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  battleship
+//
+//  Created by Theodore Abshire on 12/13/15.
+//  Copyright © 2015 Roman, Theodore, and Trub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
