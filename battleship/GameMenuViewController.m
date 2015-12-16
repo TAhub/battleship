f39ccabe42298b761f94805ae110f372b47323c6
matthew weintrub <heytrub@gmail.com>
2015-12-16 13:55:57 -0800

    adds PFloginViewController, creates user class to sign up and login in authenticated users

---
+++ battleship/GameMenuViewController.m
@@ -0,0 +1,57 @@
+//
+//  GameMenuViewController.m
+//  battleship
+//
+//  Created by Matthew Weintrub on 12/16/15.
+//  Copyright © 2015 Roman, Theodore, and Trub. All rights reserved.
+//
+
+#import "GameMenuViewController.h"
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+
+@interface GameMenuViewController () <PFLogInViewControllerDelegate>
+
+@end
+
+@implementation GameMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //add parse login
+    [self login];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Parse
+
+- (void)login {
+    PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
+    logInViewController.delegate = self;
+    [self presentViewController:logInViewController animated:YES completion:nil];
+    
+
+
+}
+
+
+- (void)signout {
+    [PFUser logOut];
+    [self login];
+}
+
+// Delegate
+
+-(void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
